d3b70d016ae50c08de4dad97b250071472c715f1
Roberto Ty <rty@futuramicmedia.com>
2014-03-21 12:51:32 -0400

    Almost there
    Got add item, checkmark and deleting work and clearing all. Need to
    figure out why some of the functioning doesn’t transfer to added item.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,31 @@
 $(document).ready(function() {
-	$('.button').submit(function() {
-		alert("It worked!");
-		//var $item = 
-	});
-});
+	// ADD ITEM
+	$("#addButton").click(function() {
+        var item = ($('#addItem').val());
+        $('#shopList').append("<li class='item'><i class='fa fa-square-o unchecked'></i>"+item+"<i class='fa fa-trash-o'></i>");
+    }); // closes add item function
+
+
+    // CHECK ITEM
+    $("i").click(function(){
+		if ($(this).hasClass('unchecked')) {
+			$(this).removeClass('fa-square-o').addClass('fa-check-square-o');
+			$(this).parent().addClass('checked');
+		}
+	}); // closes completed item function
+
+
+	// TRASH ITEM
+	$(".fa-trash-o").click(function(){
+		$(this).parent().remove();
+	}); // deletes list item
+
+	
+	
+	// CLEAR ALL ITEMS
+	$(".reset").click(function() {
+		$("li").remove();
+	}); // clears all list items
+
+
+}); // close document ready function
