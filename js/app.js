0d450e9b4beccf58ab29f33a480821147338ff7c
Roberto Ty <rty@futuramicmedia.com>
2014-03-22 12:17:31 -0400

    Working Product

--- js/app.js
+++ js/app.js
@@ -1,32 +1,40 @@
 $(document).ready(function() {
+
+	// CREATE CHECKITEM FUNCTION
+	function checkItem() {
+		// CHECK ITEM TOGGLE
+		$("i").click(function(){
+			if ($(this).parent().hasClass('unchecked') !== $(this).hasClass('fa-trash-o')) {
+				$(this).removeClass('fa-square-o').addClass('fa-check-square-o');
+				$(this).parent().removeClass('unchecked').addClass('checked');
+			}
+			else {
+				if ($(this).parent().hasClass('checked')) {
+				$(this).parent().removeClass('checked').addClass('unchecked');
+				$(this).removeClass('fa-check-square-o').addClass('fa-square-o');
+				}
+			}
+		}); // closes completed item function
+
+
+		// TRASH ITEM
+		$(".fa-trash-o").click(function(){
+			$(this).parent().remove();
+		}); // deletes list item
+	}
+
+	checkItem();
+
 	// ADD ITEM
 	$("#addButton").click(function() {
+        checkItem();
         var item = ($('#addItem').val());
-        $('#shopList').append("<li><i class='fa fa-square-o unchecked'></i>"+item+"<i class='fa fa-trash-o'></i>");
+        $('#shopList').append("<li class='unchecked'><i class='fa fa-square-o'></i>"+item+"<i class='fa fa-trash-o'></i></li>");
+        checkItem();
     }); // closes add item function
 
 
-	// CHECK ITEM TOGGLE
-	$("#shopList li i").click(function(){
-		if ($(this).parent().hasClass('unchecked')) {
-			$(this).removeClass('fa-square-o unchecked').addClass('fa-check-square-o');
-			$(this).parent().removeClass('unchecked').addClass('checked');
-		}
-		else {
-			if ($(this).parent().hasClass('checked')) {
-			$(this).parent().removeClass('checked').addClass('unchecked');
-			$(this).removeClass('fa-check-square-o').addClass('fa-square-o');
-			}
-		}
-	}); // closes completed item function
 
-
-	// TRASH ITEM
-	$(".fa-trash-o").click(function(){
-		$(this).parent().remove();
-	}); // deletes list item
-
-	
 	
 	// CLEAR ALL ITEMS
 	$(".reset").click(function() {
