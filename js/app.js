debe5785fead89d44b700219a82a5e980c577ffa
Roberto Ty <rty@futuramicmedia.com>
2014-03-22 11:06:35 -0400

    tried to fix function loop
    not working

--- js/app.js
+++ js/app.js
@@ -1,33 +1,39 @@
 $(document).ready(function() {
+
+	// =========================
+	// CREATE CHECKITEM FUNCTION
+	function checkItem () {
+		$("#shopList li i").click(function(){
+			if ($(this).hasClass('unchecked')) {
+				$(this).removeClass('fa-square-o unchecked').addClass('fa-check-square-o');
+				$(this).parent().removeClass('unchecked').addClass('checked');
+			}
+			else {
+				if ($(this).parent().hasClass('checked')) {
+				$(this).parent().removeClass('checked').addClass('unchecked');
+				$(this).removeClass('fa-check-square-o').addClass('fa-square-o');
+				}
+			}
+		}); // closes completed item function
+
+	// RUN CHECKITEM
+	$(document).checkItem();
+
 	// ADD ITEM
 	$("#addButton").click(function() {
         var item = ($('#addItem').val());
-        $('#shopList').append("<li class='item'><i class='fa fa-square-o unchecked'></i>"+item+"<i class='fa fa-trash-o'></i>");
+        $('#shopList').append("<li><i class='fa fa-square-o unchecked'></i>"+item+"<i class='fa fa-trash-o'></i>");
+        checkItem (this);
     }); // closes add item function
 
-
-    // CHECK ITEM TOGGLE
-    $("i").click(function(){
-		if ($(this).hasClass('unchecked')) {
-			$(this).removeClass('fa-square-o unchecked').addClass('fa-check-square-o');
-			$(this).parent().removeClass('unchecked').addClass('checked');
-		}
-		else {
-			if ($(this).parent().hasClass('checked')) {
-			$(this).parent().removeClass('checked').addClass('unchecked');
-			$(this).removeClass('fa-check-square-o').addClass('fa-square-o');
-			}
-		}
-	}); // closes completed item function
-
-
 	// TRASH ITEM
-	$(".fa-trash-o").click(function(){
-		$(this).parent().remove();
-	}); // deletes list item
+		$(".fa-trash-o").click(function(){
+			$(this).parent().remove();
+			}); // deletes list item
+	} // closes checkItem function
 
 	
-	
+	//================
 	// CLEAR ALL ITEMS
 	$(".reset").click(function() {
 		$("li").remove();
