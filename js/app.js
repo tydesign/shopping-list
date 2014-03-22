3dc5f01df6571332eb3ae8de78594478996fea1d
Roberto Ty <rty@futuramicmedia.com>
2014-03-22 11:30:51 -0400

    BROKEN

--- js/app.js
+++ js/app.js
@@ -1,30 +1,37 @@
 $(document).ready(function() {
+
+	// DEFINTE CHECKITEM
+	function checkItem() {
+		// CHECK ITEM TOGGLE
+		$("#shopList li i").click(function(){
+			if ($(this).hasClass('unchecked')) {
+				$(this).removeClass('fa-square-o unchecked').addClass('fa-check-square-o');
+				$(this).parent().removeClass('unchecked').addClass('checked');
+			}
+			else {
+				if ($(this).parent().hasClass('checked')) {
+				$(this).parent().removeClass('checked').addClass('unchecked');
+				$(this).removeClass('fa-check-square-o').addClass('fa-square-o');
+				}
+			}
+		}); // closes completed item function
+
+
 	// ADD ITEM
 	$("#addButton").click(function() {
         var item = ($('#addItem').val());
         $('#shopList').append("<li><i class='fa fa-square-o unchecked'></i>"+item+"<i class='fa fa-trash-o'></i>");
+        // checkItem();
     }); // closes add item function
 
 
-	// CHECK ITEM TOGGLE
-	$("#shopList li i").click(function(){
-		if ($(this).hasClass('unchecked')) {
-			$(this).removeClass('fa-square-o unchecked').addClass('fa-check-square-o');
-			$(this).parent().removeClass('unchecked').addClass('checked');
-		}
-		else {
-			if ($(this).parent().hasClass('checked')) {
-			$(this).parent().removeClass('checked').addClass('unchecked');
-			$(this).removeClass('fa-check-square-o').addClass('fa-square-o');
-			}
-		}
-	}); // closes completed item function
 
+		// TRASH ITEM
+		$(".fa-trash-o").click(function(){
+			$(this).parent().remove();
+		}); // deletes list item		
+	}
 
-	// TRASH ITEM
-	$(".fa-trash-o").click(function(){
-		$(this).parent().remove();
-	}); // deletes list item
 
 	
 	
